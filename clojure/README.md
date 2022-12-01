34e22bbf0b28e3cac55674635d52836595179c84
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-12-01 13:06:01 -0500

    Update dependencies and README

--- clojure/README.md
+++ clojure/README.md
@@ -1,1 +1,18 @@
 # aoc
+Advent of Code Clojure Solutions
+
+### New Problem Files
+
+Run the `advent` command with the year and the day. 
+
+    lein advent 2022 1 
+
+Files will be created in the src and spec folders under a directory for that year.
+
+### Specs
+
+    # Run specs once
+    lein spec
+
+    # Run specs automatically
+    lein spec -a
--- clojure/project.clj
+++ clojure/project.clj
@@ -1,7 +1,7 @@
 (defproject aoc "1.0.0-SNAPSHOT"
   :description "Solutions to https://adventofcode.com"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
-  :plugins [[speclj "3.3.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[speclj "3.4.1"]]}}
+  :plugins [[speclj "3.4.1"]]
   :test-paths ["spec/"]
-  :aliases {"aoc" ["run" "-m" "aoc.generator"]})
+  :aliases {"advent" ["run" "-m" "aoc.generator"]})
