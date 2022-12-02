92d07c97069ab556467ef6f48a18690416d30897
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-12-01 21:45:26 -0500

    Solve Day 1

---
+++ ruby/lib/2022/day_01.rb
@@ -0,0 +1,22 @@
+def rations_to_calories(rations)
+  rations.split("\n").map(&:to_i).sum
+end
+
+def elves_to_calories(elves)
+  elves
+    .split("\n\n")
+    .map{|elf_rations| rations_to_calories elf_rations}
+end
+
+def part_1(elves)
+  return 0 if elves.empty?
+  elves_to_calories(elves).max
+end
+
+def part_2(elves)
+  return 0 if elves.empty?
+  elves_to_calories(elves)
+    .sort_by {|calories| -calories}
+    .take(3)
+    .sum
+end
