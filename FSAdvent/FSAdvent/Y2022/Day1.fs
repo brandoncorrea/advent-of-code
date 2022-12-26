d1d35e08be4987467450acad6926352d1c00b43b
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-12-26 16:10:10 -0500

    Change namespace to module

--- FSAdvent/FSAdvent/Y2022/Day1.fs
+++ FSAdvent/FSAdvent/Y2022/Day1.fs
@@ -1,25 +1,23 @@
-﻿namespace FSAdvent.Y2022
+﻿module FSAdvent.Y2022.Day1
 
 open System
 
-module Day1 =
+let intOrDefault (s:string) =
+    match Int32.TryParse s with
+        | _, i -> i
+        | _ -> 0
 
-    let intOrDefault (s:string) =
-        match Int32.TryParse s with
-            | _, i -> i
-            | _ -> 0
+let elfToCalories (s:string) =
+    s.Split("\n")
+    |> Seq.map intOrDefault
+    |> Seq.sum
 
-    let elfToCalories (s:string) =
-        s.Split("\n")
-        |> Seq.map intOrDefault
-        |> Seq.sum
+let topElfCalories (n:int) (s:string) =
+    s.Split("\n\n")
+    |> Seq.map elfToCalories
+    |> Seq.sortDescending
+    |> Seq.truncate n
+    |> Seq.sum
 
-    let topElfCalories (n:int) (s:string) =
-        s.Split("\n\n")
-        |> Seq.map elfToCalories
-        |> Seq.sortDescending
-        |> Seq.truncate n
-        |> Seq.sum
-
-    let part1 = topElfCalories 1
-    let part2 = topElfCalories 3
+let part1 = topElfCalories 1
+let part2 = topElfCalories 3
--- FSAdvent/FSAdvent/Y2022/Day2.fs
+++ FSAdvent/FSAdvent/Y2022/Day2.fs
@@ -1,37 +1,33 @@
-﻿
-namespace FSAdvent.Y2022
+﻿module FSAdvent.Y2022.Day2
 
-open System
+let part1Scores =
+    ["A X", 4;
+     "A Y", 8;
+     "A Z", 3;
+     "B X", 1;
+     "B Y", 5;
+     "B Z", 9;
+     "C X", 7;
+     "C Y", 2;
+     "C Z", 6]
+    |> Map.ofList
 
-module Day2 =
-    let part1Scores =
-        ["A X", 4;
-         "A Y", 8;
-         "A Z", 3;
-         "B X", 1;
-         "B Y", 5;
-         "B Z", 9;
-         "C X", 7;
-         "C Y", 2;
-         "C Z", 6]
-        |> Map.ofList
+let part2Scores =
+    ["A X", 3;
+     "A Y", 4;
+     "A Z", 8;
+     "B X", 1;
+     "B Y", 5;
+     "B Z", 9;
+     "C X", 2;
+     "C Y", 6;
+     "C Z", 7]
+    |> Map.ofList
 
-    let part2Scores =
-        ["A X", 3;
-         "A Y", 4;
-         "A Z", 8;
-         "B X", 1;
-         "B Y", 5;
-         "B Z", 9;
-         "C X", 2;
-         "C Y", 6;
-         "C Z", 7]
-        |> Map.ofList
+let rpcScore (scores:Map<string, int>) (s:string) =
+    s.Split("\n")
+    |> Seq.choose scores.TryFind
+    |> Seq.sum
 
-    let rpcScore (scores:Map<string, int>) (s:string) =
-        s.Split("\n")
-        |> Seq.choose scores.TryFind
-        |> Seq.sum
-
-    let part1 = rpcScore part1Scores
-    let part2 = rpcScore part2Scores
+let part1 = rpcScore part1Scores
+let part2 = rpcScore part2Scores
