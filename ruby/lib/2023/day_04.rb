221ded61480500ca5b9e39c4db4a507a2c3ab03e
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-12-09 01:08:00 -0600

    Solve 2023 day 4 part 1

--- ruby/lib/2023/day_04.rb
+++ ruby/lib/2023/day_04.rb
@@ -1,12 +1,22 @@
 class Year_2023_Day_4
   class << self
     def part_1(n)
-      score_card n
+      n.split("\n")
+       .map(&method(:score_card))
+       .sum
     end
 
     def score_card(card)
-      pow = card.scan(/\d/).uniq.length - 1
-      pow >= 0 ? 2 * pow : 0
+      _, winning, mine = card.split(/[:|]/)
+      my_numbers = digits mine
+      matches = digits(winning)
+                  .filter(&my_numbers.method(:include?))
+                  .length
+      matches.zero? ? 0 : 2 ** (matches - 1)
+    end
+
+    def digits(s)
+      s.scan(/\d+/).map(&:to_i)
     end
 
     # def part_2(n) end
--- ruby/spec/2023/day_04_spec.rb
+++ ruby/spec/2023/day_04_spec.rb
@@ -9,25 +9,33 @@ describe "Advent 2023 Day 4" do
   let(:advent) { Year_2023_Day_4 }
 
   context "Part 1" do
-    # it "no cards" do
-    #   expect(advent.part_1 "").to eq 0
-    # end
-    #
-    # it "one with one matching number" do
-    #   expect(advent.part_1 "Card 1: 1 | 1").to eq 1
-    # end
-    #
-    # it "one with two matching numbers" do
-    #   expect(advent.part_1 "Card 1: 1 2 | 1 2").to eq 2
-    # end
+    it "no cards" do
+      expect(advent.part_1 "").to eq 0
+    end
 
-    # it "sample data" do
-    #   expect(advent.part_1 sample_data).to eq -1
-    # end
+    it "one matching number" do
+      expect(advent.part_1 "Card 1: 1 | 1").to eq 1
+    end
 
-    # it "input data" do
-    #   expect(advent.part_1 input_data).to eq -1
-    # end
+    it "two matching numbers" do
+      expect(advent.part_1 "Card 1: 1 2 | 1 2").to eq 2
+    end
+
+    it "three matching numbers" do
+      expect(advent.part_1 "Card 1: 1 2 3 | 1 2 3").to eq 4
+    end
+
+    it "three of five matching numbers" do
+      expect(advent.part_1 "Card 1: 1 2 3 4 5 | 2 4 5 6 10").to eq 4
+    end
+
+    it "sample data" do
+      expect(advent.part_1 sample_data).to eq 13
+    end
+
+    it "input data" do
+      expect(advent.part_1 input_data).to eq 25174
+    end
   end
 
   # context "Part 2" do
