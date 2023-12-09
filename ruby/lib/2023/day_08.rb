6c2179c435ef910d6a50274c43dbe3084d1c0e34
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2023-12-08 23:56:08 -0600

    WIP: 2023 day 8 part 2

--- ruby/lib/2023/day_08.rb
+++ ruby/lib/2023/day_08.rb
@@ -1,22 +1,19 @@
 class Year_2023_Day_8
   class << self
     def part_1(n)
-      pattern, _, *node_lines = n.split("\n")
-      nodes = node_lines.map(&method(:decode_node))
-      node_lookup = nodes.reduce({}, &method(:add_node))
+      traverse_nodes(n, :init_part_1, :all_z?)
+    end
 
-      steps = 0
-      node = 'AAA'
-      pattern.each_char.cycle do |step|
-        return steps if node == 'ZZZ'
-        index = step == 'L' ? 0 : 1
-        node = node_lookup[node][index]
-        steps += 1
-      end
+    def init_part_1(_)
+      ['AAA']
+    end
+
+    def all_z?(node)
+      node == 'ZZZ'
     end
 
     def decode_node(line)
-      node, left, right = line.scan(/[a-zA-Z]+/)
+      node, left, right = line.scan(/[a-zA-Z\d]+/)
       [node, [left, right]]
     end
 
@@ -25,6 +22,32 @@ class Year_2023_Day_8
       nodes
     end
 
-    # def part_2(n) end
+    def part_2(n)
+      traverse_nodes(n, :nodes_ending_with_a, :end_with_z?)
+    end
+
+    def nodes_ending_with_a(lookup)
+      lookup.keys.filter { |k| k.end_with?('A') }
+    end
+
+    def end_with_z?(node)
+      node.end_with?('Z')
+    end
+
+    def traverse_nodes(n, init, is_satisfied)
+      pattern, _, *node_lines = n.split("\n")
+      lookup = node_lines
+                 .map(&method(:decode_node))
+                 .reduce({}, &method(:add_node))
+      step_indices = pattern.each_char.map { |step| step == 'L' ? 0 : 1 }
+
+      steps = 0
+      state_nodes = send(init, lookup).uniq
+      step_indices.cycle do |step|
+        return steps if state_nodes.all?(&method(is_satisfied))
+        state_nodes = state_nodes.map { |node| lookup[node][step] }.uniq
+        steps += 1
+      end
+    end
   end
 end
--- ruby/spec/2023/day_08_spec.rb
+++ ruby/spec/2023/day_08_spec.rb
@@ -51,17 +51,27 @@ ZZZ = (ZZZ, ZZZ)"
     end
   end
 
-  # context "Part 2" do
-  #   it "Solves Part 2" do
-  #     expect(advent.part_2 "").to eq -1
-  #   end
+  context "Part 2" do
+    # it "Solves Part 2" do
+    #   expect(advent.part_2 "").to eq -1
+    # end
 
-  #  # it "sample data" do
-  #  #   expect(advent.part_2 sample_data).to eq -1
-  #  # end
+    it "sample data" do
+      input = "LR
 
-  #  # it "input data" do
-  #  #   expect(advent.part_2 input_data).to eq -1
-  #  # end
-  # end
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"
+      expect(advent.part_2 input).to eq 6
+    end
+
+    it "input data" do
+      # expect(advent.part_2 input_data).to eq -1
+    end
+  end
 end
