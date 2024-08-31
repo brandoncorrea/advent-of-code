2ff74a4941b35333a521c8064bd07f8af335a9a8
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-08-31 13:07:36 -0500

    Refactoring

--- Godot/spec/2015/day_1/part_1_spec.gd
+++ Godot/spec/2015/day_1/part_1_spec.gd
@@ -1,7 +1,7 @@
 extends "res://addons/gspec/gspec.gd"
 
 var SpecHelper = preload("res://spec/spec_helper.gd")
-
+var puzzle_input = SpecHelper.puzzle_input(2015, 1)
 var sut = preload("res://src/2015/day_1.gd").new()
 
 func test_going_nowhere():
@@ -20,5 +20,4 @@ func test_garbage_is_skipped():
 	should_eq(-1, sut.part_1("(ab ))"))
 
 func test_puzzle_input():
-	var input = SpecHelper.puzzle_input(2015, 1)
-	should_eq(232, sut.part_1(input))
+	should_eq(232, sut.part_1(puzzle_input))
--- Godot/spec/2015/day_1/part_2_spec.gd
+++ Godot/spec/2015/day_1/part_2_spec.gd
@@ -1,7 +1,7 @@
 extends "res://addons/gspec/gspec.gd"
 
 var SpecHelper = preload("res://spec/spec_helper.gd")
-
+var puzzle_input = SpecHelper.puzzle_input(2015, 1)
 var sut = preload("res://src/2015/day_1.gd").new()
 
 func test_empty_input():
@@ -17,5 +17,4 @@ func test_enters_basement_on_third_move():
 	should_eq(3, sut.part_2("())"))
 
 func test_puzzle_input():
-	var input = SpecHelper.puzzle_input(2015, 1)
-	should_eq(1783, sut.part_2(input))
+	should_eq(1783, sut.part_2(puzzle_input))
