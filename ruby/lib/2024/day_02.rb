044b0ad614741462fc846147e9a7967fcfd3c167
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2024-12-02 21:48:09 -0600

    Refactor 2024 day 2

--- ruby/lib/2024/day_02.rb
+++ ruby/lib/2024/day_02.rb
@@ -1,14 +1,14 @@
 class Year_2024_Day_2
   class << self
     def part_1(n)
-      safe_reports :safe?, n
+      count_reports_by :safe?, n
     end
 
     def part_2(n)
-      safe_reports :tolerable?, n
+      count_reports_by :tolerable?, n
     end
 
-    def safe_reports(pred, n)
+    def count_reports_by(pred, n)
       n.chomp_lines
        .map(&:scan_ints)
        .reject(&:empty?)
@@ -16,27 +16,25 @@ class Year_2024_Day_2
     end
 
     def tolerable?(report)
-      report.partition
-            .with_index
-            .map { |_, index| report.remove_at index }
+      report.size
+            .times
+            .map { |index| report.remove_at index }
             .any?(&method(:safe?))
     end
 
     def safe?(report)
-      differences = report.each_cons(2).map(&method(:difference))
-      differences.all?(&method(:gradual?)) && progressive?(differences)
+      differences = report.each_cons(2).map { |(a, b)| a - b }
+      gradual?(differences) && progressive?(differences)
     end
 
-    def gradual?(n)
-      n.abs <= 3
+    def gradual?(differences)
+      differences
+        .map(&:abs)
+        .all?(&:<=.with(3))
     end
 
-    def progressive?(coll)
-      coll.all?(&:positive?) || coll.all?(&:negative?)
-    end
-
-    def difference((a, b))
-      a - b
+    def progressive?(differences)
+      differences.all?(&:positive?) || differences.all?(&:negative?)
     end
   end
 end
